ffe69b6ad72534529e223913093a8053e320c9fc
Basilsabu10 <basilsabu937@gmail.com>
2026-03-27 03:21:40 +0530

    chore: configure GitHub Pages deployment with SPA routing

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App = () => {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/intervew">
       <Routes>
         {/* public routes */}
         <Route element={<PublicLayout />}>
