87eea9d5bc3887227781d4ae733ee03ef14331ff
Basilsabu10 <basilsabu937@gmail.com>
2026-03-27 01:27:52 +0530

    Configure GitHub Pages deployment via Actions and HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import { PublicLayout } from "@/layouts/public-layout";
 import AuthenticationLayout from "@/layouts/auth-layout";
